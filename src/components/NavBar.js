0fe44d9e9deb02a671d94a9cf1a9ac4a91b25142
Jeremias-Garcia <jeremias.garcia@i2t-sa.com.ar>
2022-03-21 16:54:57 -0300

    Desafío: Menú e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import './NavBar.css'
+const NavBar = () => {
+    return(
+        <div>
+            <h1>Esencia Tienda Online</h1>
+            <nav>
+                <a href="#">Productos de limpieza</a>
+                <a href="#">Aromas</a>
+                <a href="#">Hornitos</a>
+                <a href="#">Lámparas de sal</a>
+                <a href="#">Saphirus</a>
+                <a href="#">Sagrada madre</a>
+                <a href="#">Sahumerios premium</a>
+                <a href="#">Portasahumerios y sahumadores</a>
+            </nav>
+            <button>Inicio</button>
+        </div>
+    );
+};
+
+export default NavBar;
