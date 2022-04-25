c242b3237a3689cbd93c35565a513ba63aeda721
Jeremias-Garcia <jeremias.garcia@i2t-sa.com.ar>
2022-04-25 19:35:41 -0300

    Desafío: Cart View - Correccion Carrito Vacio

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import { CartContext } from "../context/CartContext";
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
+import { NavLink} from 'react-router-dom';
 
 export const Cart = () => {
     const {cart, total, removeItem} = useContext(CartContext);
@@ -12,7 +13,7 @@ export const Cart = () => {
     return(
         <div className="card">
             {/* <h4>{title}:{quantity}</h4> */}
-            {cart.map(item => <div key={item.id}><p>{item.title}: {item.quantity}</p> <button onClick={()=>handlerClick(item.id)}>X</button></div>)}
+            {(total !== 0) ? cart.map(item => <div key={item.id}><p>{item.title}: {item.quantity}</p> <button onClick={()=>handlerClick(item.id)}>X</button></div>) : <><h4>Tu carrito está vacío</h4><NavLink to="/"><button>Volver al inicio</button></NavLink></>}
             {(total !== 0 && <h3>Total: {total}</h3>)}
         </div>
     );
